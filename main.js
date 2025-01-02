0ae4adeb2dbda1489b9466029294c0eefbc7220c
Mario Triana <mariotriana1978@gmail.com>
2025-01-01 23:21:36 -0500

    npm run build...

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import { cleanLocalStorage } from "./src/utilities/utils.js"
 
 
 // Base Path in PRODUCTION
-export const basePath = "/Recipe-Calculator";
+// export const basePath = "/Recipe-Calculator";
 
 // Base Path in DEVELOPMENT
-// export const basePath = "http://localhost:5173/";
+export const basePath = "http://localhost:5173/";
 
 cleanLocalStorage()
